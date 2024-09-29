12187ceb3987431d8b49a89d6e149f58909d9914
harsh-patil-3131 <patilh4004@gmail.com>
2024-09-29 19:52:19 +0530

    Add files via upload

--- readcsv.c
+++ readcsv.c
@@ -28,7 +28,33 @@ void init_fin(final *l){
 	l->last=NULL;
 	return;
 }
-void import_csv(final *l, char *file){
+void reverse(char str[], int len){
+	int i=0;
+	char ch;
+	while(i<len/2){
+		ch=str[i];
+		str[i]=str[len-i-1];
+		str[len-i-1]=ch;
+		i++;
+	}
+}
+char *sitoa(int a, char* buffer){
+	int i=0, rem;
+	if(a==0){
+		buffer[0]='0';
+		buffer[1]='\0';
+		return buffer;
+	}
+	while(a!=0){
+		rem=a%10;
+		buffer[i++]=rem + '0';
+		a=a/10;
+	}
+	buffer[i]='\0';
+	reverse(buffer, i);
+	return buffer;
+}
+void import_data(final *l, char *file){ //reads file given into data
 	char line[50], ch;
 	int i;
 	FILE *fp;
@@ -65,19 +91,65 @@ void import_csv(final *l, char *file){
 	}
 	fclose(fp);
 }
+//In linked list, amount,d.d/d.month/d,year,comment
+//
+void export_data(final *l, char *file){
+	FILE *fp;
+	char ch, buffer[20];
+	node *p=l->start;
+	fp=fopen(file, "w");
+	while(p){
+		fputs(sitoa(p->amount, buffer), fp);
+		fputc(',', fp);
+		fputs(sitoa(p->d.d, buffer), fp);
+		fputc('/', fp);
+		fputs(sitoa(p->d.month, buffer), fp);
+		fputc('/', fp);
+		fputs(sitoa(p->d.year, buffer), fp);
+		fputc(',', fp);
+		fputs(p->comment, fp);
+		fputc('\n', fp);
+		p=p->next;
+	}
+	fclose(fp);
+}
 
+void addTransaction(final *l, int amt, char dt[], char *cmt){
+	node *nn=(node *)malloc(sizeof(node));
+	char xyz[20];
+	strcpy(xyz, dt);
+	if(l->last==NULL){
+		l->start=l->last=nn;
+		nn->next=NULL;
+	}
+	else{
+		l->last->next=nn;
+		l->last=nn;
+		nn->next=NULL;
+	}
+	nn->amount=amt;
+	l->t_id++;
+	nn->trans_ID=l->t_id;
+	nn->d.d=atoi(strtok(xyz, "/"));
+	nn->d.month=atoi(strtok(NULL, "/"));
+	nn->d.year=atoi(strtok(NULL, "\0"));
+	strcpy(nn->comment, cmt);
+}
 int main(int argc, char *argv[]){
 	final l;
 	node *p;
 	init_fin(&l);
-	//char *file="demo.csv";
-	import_csv(&l, argv[1]);
+	addTransaction(&l, 3131, "19/02/1640", "Raje1");
+	addTransaction(&l, 4141, "12/05/1822", "Raje2");
+	addTransaction(&l, 5151, "31/11/1955", "Raje3");
+	addTransaction(&l, 6161, "3/1/2010", "Raje4");
+	import_data(&l, argv[1]);
 	p=l.start;
 	while(p){
 		printf("%d,%d,%d/%d/%d,%s\n", p->amount, p->trans_ID, p->d.d, p->d.month, p->d.year, p->comment);
 		p=p->next;
 	}
+	printf("*****\n\n");
+	export_data(&l, argv[2]);
 	return 0;
 }
-
-
